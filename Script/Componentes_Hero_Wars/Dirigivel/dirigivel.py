bd67f7175a4827d6ee9516e89595f75f5a203375
Pedro Fazio <pedro.fazio.m@gmail.com>
2026-03-10 16:07:00 -0300

    Diminuição dos sleeps (falta testar)

--- Script/Componentes_Hero_Wars/Dirigivel/dirigivel.py
+++ Script/Componentes_Hero_Wars/Dirigivel/dirigivel.py
@@ -33,6 +33,8 @@ def pegar_recompensa_valquiria(coord_x, coord_y, tempo):
 
     # Clica no X do Dirigível
     #FS.mover_e_clicar(coord_x[9], coord_y[9], tempo[9])
+    PY.press('esc')
+    time.sleep(1)
 
     PY.press('esc')
     time.sleep(1)
